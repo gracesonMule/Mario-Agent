3fb418d9a342ffa493283a18de54f8dd176db0e1
Graceson J. Mule <166461187+gracesonMule@users.noreply.github.com>
2026-03-04 16:26:14 -0700

    Added check for gpu

--- agent/first_mario_agent.py
+++ agent/first_mario_agent.py
@@ -131,7 +131,13 @@ class MarioAgent:
             
         
         # If you have an Apple Silicon Mac (M1/M2/M3), you can use the MPS chip for speed!
-        self.device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+        if torch.cuda.is_available():
+            self.device = torch.device("cuda")
+        elif torch.backends.mps.is_available():
+            self.device = torch.device("mps")
+        else:
+            self.device = torch.device("cpu")
+
         self.net.to(self.device)
 
         # Learning parameters
