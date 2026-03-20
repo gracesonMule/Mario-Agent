c69c4459a8bd3b6cb916d8d9af6ee57572c63dd4
Graceson J. Mule <166461187+gracesonMule@users.noreply.github.com>
2026-03-19 18:37:55 -0700

    Load dataset from RoboFlow

--- YOLO-LSTM/load-dataset.py
+++ YOLO-LSTM/load-dataset.py
@@ -1,8 +1,8 @@
 from roboflow import Roboflow
 def main():
     rf = Roboflow(api_key="")
-    project = rf.workspace("mario-agent-workspace").project("mario-dataset-o65mb")
-    version = project.version(1)
+    project = rf.workspace("ethan-ortega-zqedq").project("mario-dataset")
+    version = project.version(6)
     dataset = version.download("yolo26")
 
 main()
