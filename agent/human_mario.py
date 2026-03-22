1190427c8302e9fa5f2c5cae6828060075d260c5
root <root@KCDUN.localdomain>
2026-03-22 13:52:59 -0700

    added stage frames for 1-2

--- agent/human_mario.py
+++ agent/human_mario.py
@@ -2,7 +2,7 @@ import gym_super_mario_bros
 from nes_py.app.play_human import play_human
 
 # Initialize the environment
-env = gym_super_mario_bros.make('SuperMarioBros-v0')
+env = gym_super_mario_bros.make('SuperMarioBros-1-2-v0')
 
 # Launch the interactive window
 play_human(env)
