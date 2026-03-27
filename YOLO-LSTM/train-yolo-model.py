4e9abdf606bf4d6de0f3a4c7a5910ddeeb041470
gracesonMule <graceson.james1@gmail.com>
2026-03-27 01:02:30 -0700

    YOLO model training results

--- YOLO-LSTM/train-yolo-model.py
+++ YOLO-LSTM/train-yolo-model.py
@@ -1,11 +1,11 @@
 from ultralytics import YOLO
 
-model = YOLO('yolo26n-obb.yaml') 
+model = YOLO('yolov8n.pt') 
 
 # Train the model
 results = model.train(
-    data='path/to/your/data.yaml', 
-    epochs=100, 
+    data='data.yaml', 
+    epochs=100_000, 
     imgsz=256, 
     batch=16,
     device=0 # Uses your primary GPU. Use 'cpu' if no GPU is available.
@@ -14,7 +14,7 @@ results = model.train(
 model = YOLO('runs/detect/train/weights/best.pt')
 
 results = model.predict(
-    source='path/to/your/unseen_test_data', 
+    source='Mario-Dataset-6/test/images', 
     conf=0.5,   # Confidence threshold: only show boxes it is at least 50% sure about
     save=True,  # Saves copies of the images with the boxes drawn on them
     show=True   # Opens a window to display the results in real-time
