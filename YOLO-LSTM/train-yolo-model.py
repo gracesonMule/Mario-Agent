07a75c306d103ba7fa86ca6dc0fa84c6d5b291ab
gracesonMule <graceson.james1@gmail.com>
2026-03-27 15:40:55 -0700

    adjusted data set

--- YOLO-LSTM/train-yolo-model.py
+++ YOLO-LSTM/train-yolo-model.py
@@ -5,16 +5,17 @@ model = YOLO('yolov8n.pt')
 # Train the model
 results = model.train(
     data='data.yaml', 
+    cfg='custom_hyp.yaml',
     epochs=100_000, 
-    imgsz=256, 
-    batch=16,
+    imgsz=640, 
+    batch=32,
     device=0 # Uses your primary GPU. Use 'cpu' if no GPU is available.
 )
 
 model = YOLO('runs/detect/train/weights/best.pt')
 
 results = model.predict(
-    source='Mario-Dataset-6/test/images', 
+    source='SuperMarioBros-3-2-v0', 
     conf=0.5,   # Confidence threshold: only show boxes it is at least 50% sure about
     save=True,  # Saves copies of the images with the boxes drawn on them
     show=True   # Opens a window to display the results in real-time
