274fc57b3e368649f38bfd29ad07dce45ad05d58
gracesonMule <graceson.james1@gmail.com>
2026-03-31 17:39:47 -0700

    updated

--- YOLO-LSTM/train-yolo-model.py
+++ YOLO-LSTM/train-yolo-model.py
@@ -6,9 +6,10 @@ model = YOLO('yolov8n.pt')
 results = model.train(
     data='synth-data.yaml', 
     cfg='custom_hyp.yaml',
-    epochs=100_000, 
+    epochs=100, 
     imgsz=640, 
-    batch=64,
+    batch=16,
+    workers=4,
     device=0 # Uses your primary GPU. Use 'cpu' if no GPU is available.
 )
 
@@ -18,5 +19,5 @@ results = model.predict(
     source='SuperMarioBros-3-2-v0', 
     conf=0.5,   # Confidence threshold: only show boxes it is at least 50% sure about
     save=True,  # Saves copies of the images with the boxes drawn on them
-    show=True   # Opens a window to display the results in real-time
+    show=False   # Opens a window to display the results in real-time
 )
